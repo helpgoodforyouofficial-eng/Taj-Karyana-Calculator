06519f9141f763b1f1faada5263477c3d2dde0a3
helpgoodforyouofficial-eng <helpgoodforyouofficial@gmail.com>
2026-02-11 23:12:17 +0500

    Update sw.js
    V7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'taj-calc-v6'; // Har baar update par v3 se v4, v5 karte rehna
+const cacheName = 'taj-calc-v7'; // Har baar update par v3 se v4, v5 karte rehna
 const assets = [
   './',
   './index.html',
@@ -34,5 +34,6 @@ self.addEventListener('fetch', e => {
     })
   );
 });
+
 
 
